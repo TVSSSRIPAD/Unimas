f8756c9f824ea86d4dc6d93c85ecbb980930ef73
TVSSSRIPAD <tss11@iitbbs.ac.in>
2021-04-12 20:27:48 +0530

    init project

---
+++ src/main/java/com/sripad/unimas/UnimasApplication.java
@@ -0,0 +1,13 @@
+package com.sripad.unimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sripad/unimas/UnimasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sripad.unimas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
